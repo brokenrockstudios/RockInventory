d046c724b0445bcdcd3444258196aeb9921b73ed
namrog84 <namrog84@gmail.com>
2025-04-18 02:46:55 -0700

    Add initial editor module

---
+++ Source/RockInventoryEditor/Private/RockInventoryEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "RockInventoryEditor.h"
+
+#define LOCTEXT_NAMESPACE "FRockInventoryEditorModule"
+
+void FRockInventoryEditorModule::StartupModule()
+{
+    
+}
+
+void FRockInventoryEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FRockInventoryEditorModule, RockInventoryEditor)
---
+++ Source/RockInventoryEditor/RockInventoryEditor.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class RockInventoryEditor : ModuleRules
+{
+    public RockInventoryEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
