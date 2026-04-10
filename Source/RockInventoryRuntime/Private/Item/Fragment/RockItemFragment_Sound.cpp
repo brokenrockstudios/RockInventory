d3bedf9b62a2b176d6c5da713616f354cb1309c5
namrog84 <namrog84@gmail.com>
2026-04-09 20:55:29 -0700

    Add sound preset support to RockItemFragment_Sound with getter methods

--- Source/RockInventoryRuntime/Private/Item/Fragment/RockItemFragment_Sound.cpp
+++ Source/RockInventoryRuntime/Private/Item/Fragment/RockItemFragment_Sound.cpp
@@ -2,3 +2,38 @@
 
 
 #include "Item/Fragment/RockItemFragment_Sound.h"
+
+const USoundBase* FRockItemFragment_Sound::GetWorldPickup() const
+{
+	if (!WorldPickup.IsNull()) { return WorldPickup.Get(); }
+	if (Preset) { return Preset->WorldPickup.Get(); }
+	return nullptr;
+}
+
+const USoundBase* FRockItemFragment_Sound::GetWorldDrop() const
+{
+	if (!WorldDrop.IsNull()) { return WorldDrop.Get(); }
+	if (Preset) { return Preset->WorldDrop.Get(); }
+	return nullptr;
+}
+
+const USoundBase* FRockItemFragment_Sound::GetInventoryPickup() const
+{
+	if (!InventoryDrop.IsNull()) { return InventoryDrop.Get(); }
+	if (Preset) { return Preset->InventoryDrop.Get(); }
+	return nullptr;
+}
+
+const USoundBase* FRockItemFragment_Sound::GetInventoryMove() const
+{
+	if (!InventoryMove.IsNull()) { return InventoryMove.Get(); }
+	if (Preset) { return Preset->InventoryMove.Get(); }
+	return nullptr;
+}
+
+const USoundBase* FRockItemFragment_Sound::GetInventoryDrop() const
+{
+	if (!InventoryDrop.IsNull()) { return InventoryDrop.Get(); }
+	if (Preset) { return Preset->InventoryDrop.Get(); }
+	return nullptr;
+}
--- Source/RockInventoryRuntime/Public/Item/Fragment/RockItemFragment_Sound.h
+++ Source/RockInventoryRuntime/Public/Item/Fragment/RockItemFragment_Sound.h
@@ -7,6 +7,35 @@
 
 #include "RockItemFragment_Sound.generated.h"
 
+
+// Enable re-usable
+UCLASS(BlueprintType, Const)
+class ROCKINVENTORYRUNTIME_API URockItemSoundPreset : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	/** A sound to place when picking up this item from the world */
+	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "Gameplay"))
+	TSoftObjectPtr<USoundBase> WorldPickup = nullptr;
+
+	/** A sound to play when dropping the item into the world */
+	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "Gameplay"))
+	TSoftObjectPtr<USoundBase> WorldDrop = nullptr;
+
+	/** When picking up an item onto the mouse during a dragdrop operation*/
+	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "UI"))
+	TSoftObjectPtr<USoundBase> InventoryPickup = nullptr;
+
+	/** A potentially looping type sound to play while active in a dragdrop operation. (e.g. an animal or bug or orb might emit sounds) */
+	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "UI"))
+	TSoftObjectPtr<USoundBase> InventoryMove = nullptr;
+
+	/** At the conclusion of a dragdrop operation when placing it down, perhaps a deeper thud sound */
+	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "UI"))
+	TSoftObjectPtr<USoundBase> InventoryDrop = nullptr;
+};
+
+
 /** 
  * This fragment allows you to override specify various sounds related to the item, such as pickup and drop sounds in the world, 
  * and sounds for inventory interactions like grabbing and placing the item during drag-and-drop operations.
@@ -17,10 +46,11 @@ USTRUCT(BlueprintType)
 struct ROCKINVENTORYRUNTIME_API FRockItemFragment_Sound : public FRockItemFragment
 {
 	GENERATED_BODY()
-
 public:
 	//////////////////////////////////////////////////////////////////////////
 	/// Sound
+	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "Gameplay,UI"))
+	TObjectPtr<URockItemSoundPreset> Preset = nullptr;
 
 	/** A sound to place when picking up this item from the world */
 	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "Gameplay"))
@@ -41,4 +71,11 @@ public:
 	/** At the conclusion of a dragdrop operation when placing it down, perhaps a deeper thud sound */
 	UPROPERTY(EditDefaultsOnly, Category = "Sound", meta = (AssetBundles= "UI"))
 	TSoftObjectPtr<USoundBase> InventoryDrop = nullptr;
+
+	// Prefer to use these over direct access so we can reconcile preset vs override
+	const USoundBase* GetWorldPickup() const;
+	const USoundBase* GetWorldDrop() const;
+	const USoundBase* GetInventoryPickup() const;
+	const USoundBase* GetInventoryMove() const;
+	const USoundBase* GetInventoryDrop() const;
 };
