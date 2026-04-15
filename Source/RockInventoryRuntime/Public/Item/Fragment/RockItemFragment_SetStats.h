fe0e39699562b617c0fd08d3b24c58fe8aea403b
namrog84 <namrog84@gmail.com>
2026-04-15 00:24:05 -0700

    formatting and push _SetStats to end of list.

--- Source/RockInventoryRuntime/Public/Item/Fragment/RockItemFragment_SetStats.h
+++ Source/RockInventoryRuntime/Public/Item/Fragment/RockItemFragment_SetStats.h
@@ -20,14 +20,17 @@ struct ROCKINVENTORYRUNTIME_API FRockItemFragment_SetStats : public FRockItemFra
 
 	UPROPERTY(EditDefaultsOnly, Category=Equipment, meta=(Tooltip="This is used to set initial stats on an item's instance if there is one when it's created."))
 	TMap<FGameplayTag, int32> InitialItemStats;
-	
+
 	// Better way to do this? or should we validate that the Inventory has a matching GameplayTag?
 	// Or perhaps we could do an 'editor' trick to only show this if it has a CustomValue1 GameplayTag and/or even show it to the user
 	UPROPERTY(EditDefaultsOnly, Category=Equipment)
 	int32 CustomValue1 = 0;
-	
+
 	// Better way to do this? or should we validate that the Inventory has a matching GameplayTag?
 	// Or perhaps we could do an 'editor' trick to only show this if it has a CustomValue1 GameplayTag and/or even show it to the user
 	UPROPERTY(EditDefaultsOnly, Category=Equipment)
 	int32 CustomValue2 = 0;
+
+	// Only happens once at initialization. Should be super infrequently queried. Back of the array for you!
+	virtual int32 GetSortOrder() const override { return 1000000; }
 };
